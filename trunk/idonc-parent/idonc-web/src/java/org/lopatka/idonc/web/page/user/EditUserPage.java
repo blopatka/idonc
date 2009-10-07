28f7b8f97850e41a8a3e4c551e1d89a3c48c5db9
bartlomiej.lopatka <bartlomiej.lopatka@34a9271f-e61a-0410-a1e1-eb5a51ea23ef>
2009-10-07 09:38:08 +0000

    poprawki związane z logowaniem użytkownika, wyświetlaniem listy użytkowników i edytowaniem użytkownika

--- trunk/idonc-parent/idonc-web/src/java/org/lopatka/idonc/web/page/user/EditUserPage.java
+++ trunk/idonc-parent/idonc-web/src/java/org/lopatka/idonc/web/page/user/EditUserPage.java
@@ -7,93 +7,151 @@ import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-import org.lopatka.idonc.web.IdoncSession;
+import org.lopatka.idonc.model.user.Address;
 import org.lopatka.idonc.model.user.IdoncUser;
-import org.lopatka.idonc.web.page.component.BasePage;
 import org.lopatka.idonc.service.IdoncService;
+import org.lopatka.idonc.web.IdoncSession;
+import org.lopatka.idonc.web.page.component.BasePage;
 
 @AuthorizeInstantiation("ADMIN")
-public class EditUserPage extends BasePage{
+public class EditUserPage extends BasePage {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-	@SpringBean(name="idoncService")
-	private IdoncService idoncService;
-	
-	private IdoncUser editedUser;
-	private String oldPassword;
-	private String password;
-	private String rePassword;
-	
 	public EditUserPage() {
-		
-		editedUser = IdoncSession.get().getLoggedUser().getUser();
-		
 		initLayout();
 	}
-	
+
 	private void initLayout() {
-		Form form = new Form("editUserForm");
-		form.add(new Label("usernameInput", new PropertyModel(this, "editedUser.userName")));
-		form.add(new PasswordTextField("oldPasswordInput", new PropertyModel(this, "oldPassword")));
-		form.add(new PasswordTextField("passwordInput", new PropertyModel(this, "password")));
-		form.add(new PasswordTextField("rePasswordInput", new PropertyModel(this, "rePassword")));
-		form.add(new TextField("firstNameInput", new PropertyModel(this, "editedUser.firstName")));
-		form.add(new TextField("lastNameInput", new PropertyModel(this, "editedUser.lastName")));
-		form.add(new TextField("emailInput", new PropertyModel(this, "editedUser.address.email")));
-		form.add(new TextField("websiteInput", new PropertyModel(this, "editedUser.address.website")));
-		form.add(new TextField("streetInput", new PropertyModel(this, "editedUser.address.street")));
-		form.add(new TextField("houseNumberInput", new PropertyModel(this, "editedUser.address.houseNumber")));
-		form.add(new TextField("cityInput", new PropertyModel(this, "editedUser.address.city")));
-		form.add(new TextField("zipCodeInput", new PropertyModel(this, "editedUser.address.zipCode")));
-		form.add(new TextField("countryInput", new PropertyModel(this, "editedUser.address.country")));
+		add(new EditUserForm("editUserForm"));
+	}
 
-		form.add(new Button("acceptButton") {
+	private class EditUserForm extends Form {
 
-			private static final long serialVersionUID = 1L;
+		private static final long serialVersionUID = 1L;
 
-			@Override
-			public void onSubmit() {
-				//TODO check if username exists
-//				if(password.equals(rePassword)) {
-//					idoncService.registerUser(user, password);
+		@SpringBean(name = "idoncService")
+		private IdoncService idoncService;
+
+		private IdoncUser editedUser;
+
+		private PasswordTextField passwordInput;
+		private PasswordTextField rePasswordInput;
+		private TextField firstNameInput;
+		private TextField lastNameInput;
+		private TextField emailInput;
+		private TextField websiteInput;
+		private TextField streetInput;
+		private TextField houseNumberInput;
+		private TextField cityInput;
+		private TextField zipCodeInput;
+		private TextField countryInput;
+
+		public EditUserForm(String id) {
+			super(id);
+			editedUser = IdoncSession.get().getLoggedUser().getUser();
+
+			add(new Label("usernameInput", new PropertyModel(this,
+					"editedUser.userName")));
+			passwordInput = new PasswordTextField("passwordInput",
+					new Model(""));
+			add(passwordInput);
+			rePasswordInput = new PasswordTextField("rePasswordInput",
+					new Model(""));
+			add(rePasswordInput);
+			firstNameInput = new TextField("firstNameInput", new PropertyModel(
+					this, "editedUser.firstName"));
+			add(firstNameInput);
+			lastNameInput = new TextField("lastNameInput", new PropertyModel(
+					this, "editedUser.lastName"));
+			add(lastNameInput);
+			emailInput = new TextField("emailInput", new PropertyModel(this,
+					"editedUser.address.email"));
+			add(emailInput);
+			websiteInput = new TextField("websiteInput", new PropertyModel(
+					this, "editedUser.address.website"));
+			add(websiteInput);
+			streetInput = new TextField("streetInput", new PropertyModel(this,
+					"editedUser.address.street"));
+			add(streetInput);
+			houseNumberInput = new TextField("houseNumberInput",
+					new PropertyModel(this, "editedUser.address.houseNumber"));
+			add(houseNumberInput);
+			cityInput = new TextField("cityInput", new PropertyModel(this,
+					"editedUser.address.city"));
+			add(cityInput);
+			zipCodeInput = new TextField("zipCodeInput", new PropertyModel(
+					this, "editedUser.address.zipCode"));
+			add(zipCodeInput);
+			countryInput = new TextField("countryInput", new PropertyModel(
+					this, "editedUser.address.country"));
+			add(countryInput);
+
+			this.add(new Button("acceptButton") {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void onSubmit() {
+					if ((passwordInput.getModelObject() != null)
+							&& (rePasswordInput.getModelObject() != null)) {
+						if (passwordInput.getModelObject().equals(
+								rePasswordInput.getModelObject())) {
+							editedUser.setFirstName(firstNameInput.getInput());
+							editedUser.setLastName(lastNameInput.getInput());
+							Address address = editedUser.getAddress();
+							if (address == null) {
+								address = new Address();
+							}
+							address.setEmail(emailInput.getInput());
+							address.setWebsite(websiteInput.getInput());
+							address.setStreet(streetInput.getInput());
+							address.setHouseNumber(houseNumberInput.getInput());
+							address.setCity(cityInput.getInput());
+							address.setZipCode(zipCodeInput.getInput());
+							address.setCountry(countryInput.getInput());
+							editedUser.setAddress(address);
+							idoncService.editUser(editedUser, passwordInput.getModelObjectAsString());
+						}
+						setResponsePage(UserListPage.class);
+					}
+				}
+
+
+			});
+
+			this.add(new Button("backButton") {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void onSubmit() {
+					setResponsePage(UserListPage.class);
+				}
+			});
+
+//			Link accept = new Link("acceptLink") {
+//				private static final long serialVersionUID = 1L;
+//
+//				@Override
+//				public void onClick() {
+//
 //				}
-				setResponsePage(UserListPage.class);
-			}
-		});
+//			};
+//
+//			Link cancel = new Link("cancelLink") {
+//				private static final long serialVersionUID = 1L;
+//
+//				@Override
+//				public void onClick() {
+//					setResponsePage(UserListPage.class);
+//				}
+//			};
+//			add(accept);
+//			add(cancel);
 
-		form.add(new Button("cancelButton") {
+		}
 
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void onSubmit() {
-				setResponsePage(UserListPage.class);
-			}
-		});
-
-        Link accept = new Link("acceptLink") {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void onClick() {
-                setResponsePage(UserListPage.class);
-            }
-        };
-
-        Link cancel = new Link("cancelLink") {
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public void onClick() {
-                setResponsePage(UserListPage.class);
-            }
-        };
-        add(accept);
-        add(cancel);
-
-		add(form);
 	}
 }
