c7d67c15bbdbe35032a175585fe3b69a6da61cc0
bartlomiej.lopatka <opiszon@users.noreply.github.com>
2009-10-09 13:59:02 +0000

    style

--- idonc-parent/idonc-web/src/java/org/lopatka/idonc/web/page/RegisterUserPage.java
+++ idonc-parent/idonc-web/src/java/org/lopatka/idonc/web/page/RegisterUserPage.java
@@ -17,58 +17,61 @@ public class RegisterUserPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-    @SpringBean(name="idoncService")
-	private IdoncService idoncService;
-	
-	private IdoncUser user;
-	private String password;
-	private String rePassword;
+
 
 	public RegisterUserPage() {
-		Form form = new Form("registerForm");
-		form.add(new TextField("usernameInput", new PropertyModel(this, "user.userName")));
-		form.add(new PasswordTextField("passwordInput", new PropertyModel(this, "password")));
-		form.add(new PasswordTextField("rePasswordInput", new PropertyModel(this, "rePassword")));
-		form.add(new TextField("firstNameInput", new PropertyModel(this, "user.firstName")));
-		form.add(new TextField("lastNameInput", new PropertyModel(this, "user.lastName")));
-		form.add(new TextField("emailInput", new PropertyModel(this, "user.address.email")));
-		form.add(new TextField("websiteInput", new PropertyModel(this, "user.address.website")));
-		form.add(new TextField("streetInput", new PropertyModel(this, "user.address.street")));
-		form.add(new TextField("houseNumberInput", new PropertyModel(this, "user.address.houseNumber")));
-		form.add(new TextField("cityInput", new PropertyModel(this, "user.address.city")));
-		form.add(new TextField("zipCodeInput", new PropertyModel(this, "user.address.zipCode")));
-		form.add(new TextField("countryInput", new PropertyModel(this, "user.address.country")));
+		add(new RegisterUserForm("registerForm"));
+	}
 
+	private class RegisterUserForm extends Form {
 
-		Button accept = new Button("acceptButton") {
+		private static final long serialVersionUID = 1L;
 
-			private static final long serialVersionUID = -4742823374395732L;
+		@SpringBean(name="idoncService")
+		private IdoncService idoncService;
 
-			@Override
-			public void onSubmit() {
-				//TODO check if username exists
-				if(password.equals(rePassword)) {
-					idoncService.registerUser(user, password);
+		private IdoncUser user;
+		private String password;
+		private String rePassword;
+
+		public RegisterUserForm(String id) {
+			super(id);
+			add(new TextField("usernameInput", new PropertyModel(this, "user.userName")));
+			add(new PasswordTextField("passwordInput", new PropertyModel(this, "password")));
+			add(new PasswordTextField("rePasswordInput", new PropertyModel(this, "rePassword")));
+			add(new TextField("firstNameInput", new PropertyModel(this, "user.firstName")));
+			add(new TextField("lastNameInput", new PropertyModel(this, "user.lastName")));
+			add(new TextField("emailInput", new PropertyModel(this, "user.address.email")));
+			add(new TextField("websiteInput", new PropertyModel(this, "user.address.website")));
+			add(new TextField("streetInput", new PropertyModel(this, "user.address.street")));
+			add(new TextField("houseNumberInput", new PropertyModel(this, "user.address.houseNumber")));
+			add(new TextField("cityInput", new PropertyModel(this, "user.address.city")));
+			add(new TextField("zipCodeInput", new PropertyModel(this, "user.address.zipCode")));
+			add(new TextField("countryInput", new PropertyModel(this, "user.address.country")));
+
+			this.add( new Button("acceptButton") {
+				private static final long serialVersionUID = -4742823374395732L;
+
+				@Override
+				public void onSubmit() {
+					//TODO check if username exists
+					if(password.equals(rePassword)) {
+						idoncService.registerUser(user, password);
+					}
+					setResponsePage(HomePage.class);
 				}
-                
-				setResponsePage(HomePage.class);
-			}
-		};
-		form.add(accept);
+			});
+			Button backButton = new Button("backButton") {
+				private static final long serialVersionUID = 1L;
 
-
-		Button cancel = new Button("cancelButton") {
-
-			private static final long serialVersionUID = 6703091768251015771L;
-
-			@Override
-			public void onSubmit() {
-				setResponsePage(LoginPage.class);
-			}
-		};
-		form.add(cancel);
-
-		this.add(form);
+				@Override
+				public void onSubmit() {
+					setResponsePage(HomePage.class);
+				}
+			};
+			backButton.setDefaultFormProcessing(false);
+			this.add(backButton);
+		}
 	}
 }
 
