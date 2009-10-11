9e81a5ed86bda784b8e690de502d70f84248d9e1
bartlomiej.lopatka <bartlomiej.lopatka@34a9271f-e61a-0410-a1e1-eb5a51ea23ef>
2009-10-11 21:44:24 +0000

    cliens Swing Application Framework introduction

---
+++ trunk/idonc-parent/idonc-wsclient/src/java/org/lopatka/idonc/client/MainPanel.java
@@ -0,0 +1,18 @@
+package org.lopatka.idonc.client;
+
+import javax.swing.JPanel;
+
+public class MainPanel extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+	
+	public MainPanel() {
+		initComponents();
+	}
+	
+	private void initComponents() {
+		
+	}
+
+	
+}
---
+++ trunk/idonc-parent/idonc-wsclient/src/java/org/lopatka/idonc/client/MainViewFrame.java
@@ -0,0 +1,12 @@
+package org.lopatka.idonc.client;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.FrameView;
+
+public class MainViewFrame extends FrameView {
+
+	public MainViewFrame(Application application) {
+		super(application);
+	}
+
+}
