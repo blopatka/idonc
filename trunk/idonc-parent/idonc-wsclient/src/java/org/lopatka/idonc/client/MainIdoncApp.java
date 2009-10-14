3fbfa60de83c436a9d607e0101a66e54f8eaa7f5
bartlomiej.lopatka <bartlomiej.lopatka@34a9271f-e61a-0410-a1e1-eb5a51ea23ef>
2009-10-14 04:38:35 +0000

    usuniecie wyboru jezyka

--- trunk/idonc-parent/idonc-wsclient/src/java/org/lopatka/idonc/client/MainIdoncApp.java
+++ trunk/idonc-parent/idonc-wsclient/src/java/org/lopatka/idonc/client/MainIdoncApp.java
@@ -16,40 +16,6 @@ public class MainIdoncApp extends SingleFrameApplication {
 	ResourceMap resource;
 	ApplicationContext ctx;
 
-	// /**
-	// * Create the GUI and show it. For thread safety, this method should be
-	// * invoked from the event-dispatching thread.
-	// */
-	// public static void createAndShowGUI() {
-	// JFrame.setDefaultLookAndFeelDecorated(true);
-	// JFrame frame = new JFrame("idonc webstart client");
-	// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	//
-	// JLabel label = new JLabel();
-	// label.setHorizontalAlignment(JLabel.CENTER);
-	// label.setFont(label.getFont().deriveFont(Font.PLAIN));
-	// frame.getContentPane().add(label);
-	//
-	// String text = null;
-	//
-	// ApplicationContext context = new ClassPathXmlApplicationContext(
-	// "client-applicationContext.xml");
-	// IdoncService idoncService = (IdoncService) context
-	// .getBean("idoncService");
-	// ConverterService service = (ConverterService) context
-	// .getBean("converterService");
-	// Celsius cel = new Celsius();
-	// cel.setTemperatur(10);
-	// Farenheit far = (Farenheit) service.celsiusToFarenheit(cel);
-	// text = "celsius " + cel.getTemperature() + "is farenheit"
-	// + far.getTemperature();
-	//
-	// label.setText(text);
-	//
-	// frame.pack();
-	// frame.setVisible(true);
-	// }
-
 	@Override
 	protected void startup() {
 		FrameView frameView = new MainViewFrame(this);
