e8d6e9276826d05ed54976381183fdd259bd0830
bartlomiej.lopatka <opiszon@users.noreply.github.com>
2009-10-28 15:10:57 +0000

    comp

--- idonc-parent/idon-computation/src/main/java/org/idon/computation/IComputation.java
+++
@@ -1,9 +0,0 @@
-package org.idon.computation;
-
-import org.lopatka.idonc.model.data.IdoncPart;
-import org.lopatka.idonc.model.data.IdoncResult;
-
-public interface IComputation {
-
-	IdoncResult computeData(IdoncPart part);
-}
