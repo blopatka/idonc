df56b9a30d41b82ac151e1c16b0ff98b784dd7d8
bartlomiej.lopatka <bartlomiej.lopatka@34a9271f-e61a-0410-a1e1-eb5a51ea23ef>
2009-11-02 15:11:25 +0000

    usuniecie generatora danych z serwisu

--- trunk/idonc-parent/idonc-service/src/java/org/lopatka/idonc/service/GeneratorServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package org.lopatka.idonc.service;
-
-import java.io.Serializable;
-
-public class GeneratorServiceImpl implements GeneratorService, Serializable{
-
-	private static final long serialVersionUID = 1L;
-
-}
