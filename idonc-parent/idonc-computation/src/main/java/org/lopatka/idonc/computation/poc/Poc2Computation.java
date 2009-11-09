5a93e503e6ee8ca46d244bc218f3a6ff141c4c01
bartlomiej.lopatka <opiszon@users.noreply.github.com>
2009-11-09 12:51:05 +0000

    add

---
+++ idonc-parent/idonc-computation/src/main/java/org/lopatka/idonc/computation/poc/Poc2Computation.java
@@ -0,0 +1,20 @@
+package org.lopatka.idonc.computation.poc;
+
+import org.lopatka.idonc.computation.IComputation;
+import org.lopatka.idonc.model.data.IdoncPart;
+import org.lopatka.idonc.model.data.IdoncResult;
+
+public class Poc2Computation implements IComputation {
+
+	@Override
+	public IdoncResult computeData(IdoncPart part) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isResultConfirmationRequired() {
+		return false;
+	}
+
+}
