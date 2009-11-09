6f32b06c8fd5ed8cb03251bccdb58535a4196d69
bartlomiej.lopatka <bartlomiej.lopatka@34a9271f-e61a-0410-a1e1-eb5a51ea23ef>
2009-11-09 12:24:59 +0000

    move

---
+++ trunk/idonc-parent/idonc-computation/src/main/java/org/lopatka/idonc/computation/IComputation.java
@@ -0,0 +1,23 @@
+package org.lopatka.idonc.computation;
+
+import org.lopatka.idonc.model.data.IdoncPart;
+import org.lopatka.idonc.model.data.IdoncResult;
+
+public interface IComputation {
+
+	/**
+	 * metoda przeprowadzajaca obliczenia na dostarczonym pakiecie danych wejsciowych
+	 *
+	 * @param part
+	 * @return
+	 */
+	IdoncResult computeData(IdoncPart part);
+
+	/**
+	 * metoda okreslajaca, czy otrzymany wynik powinien byc powtorzony przez innego
+	 * uzytkownika, zeby zostal uznany za poprawny
+	 *
+	 * @return
+	 */
+	boolean isResultConfirmationRequired();
+}
---
+++ trunk/idonc-parent/idonc-computation/src/main/java/org/lopatka/idonc/computation/pi/PiComputation.java
@@ -0,0 +1,14 @@
+package org.idonc.computation.pi;
+
+import org.idonc.computation.IComputation;
+import org.lopatka.idonc.model.data.IdoncPart;
+import org.lopatka.idonc.model.data.IdoncResult;
+
+public class PiComputation implements IComputation {
+
+	public IdoncResult computeData(IdoncPart part) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
