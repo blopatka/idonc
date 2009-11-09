fc1dd8a77c102cd1b561be5f0269120f7c5339d4
bartlomiej.lopatka <opiszon@users.noreply.github.com>
2009-11-09 15:19:39 +0000

    dodano obsluge obliczen w watku

--- idonc-parent/idonc-computation/src/main/java/org/lopatka/idonc/computation/poc/PocComputation.java
+++ idonc-parent/idonc-computation/src/main/java/org/lopatka/idonc/computation/poc/PocComputation.java
@@ -1,14 +1,33 @@
 package org.lopatka.idonc.computation.poc;
 
+import java.util.List;
+
 import org.lopatka.idonc.computation.IComputation;
+import org.lopatka.idonc.model.data.IdoncLongData;
 import org.lopatka.idonc.model.data.IdoncPart;
 import org.lopatka.idonc.model.data.IdoncResult;
 
 public class PocComputation implements IComputation {
 
 	public IdoncResult computeData(IdoncPart part) {
-		// TODO Auto-generated method stub
-		return null;
+		List<IdoncLongData> list = part.getLongDataList();
+		long resultValue = 0;
+		for (IdoncLongData data : list) {
+			try {
+				final long val = Long.parseLong(data.getValue());
+
+				System.out.println("sleeping: "+val);
+				waiting(val);
+				System.out.println("waking");
+
+				resultValue += val;
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		IdoncResult result = new IdoncResult();
+		result.setValue(Long.toString(resultValue));
+		return result;
 	}
 
 	@Override
@@ -16,4 +35,14 @@ public class PocComputation implements IComputation {
 		return true;
 	}
 
+	private void waiting(long n) {
+
+		long t0, t1;
+
+		t0 = System.currentTimeMillis();
+
+		do {
+			t1 = System.currentTimeMillis();
+		} while (t1 - t0 < n);
+	}
 }
